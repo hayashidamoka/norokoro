b9c65493257e9f8c6646bfa1d42e0603e39c59c6
fulukawamoka <shina_yuruha@i.softbank.jp>
2018-12-04 20:57:59 +0900

    フラッシュがきれいに出てくる。 呪うボタンが押せる。

--- app/src/main/java/com/uchinokomoratte/norokoro/MainActivity.java
+++ app/src/main/java/com/uchinokomoratte/norokoro/MainActivity.java
@@ -1,13 +1,25 @@
 package com.uchinokomoratte.norokoro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView noroubutton = findViewById(R.id.norou_button);
+        noroubutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent();
+                intent.setClass(MainActivity.this,NorouActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/uchinokomoratte/norokoro/NorouActivity.java
@@ -0,0 +1,15 @@
+package com.uchinokomoratte.norokoro;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class NorouActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_norou);
+    }
+}
