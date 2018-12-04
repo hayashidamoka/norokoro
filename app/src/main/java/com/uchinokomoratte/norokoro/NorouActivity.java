9a7f5e2e0c64b575221b9d8bb2b541b153ca3fae
fulukawamoka <shina_yuruha@i.softbank.jp>
2018-12-04 20:59:33 +0900

    フラッシュがきれいに出てくる。 呪うボタンが押せる。

--- app/src/main/java/com/uchinokomoratte/norokoro/NorouActivity.java
+++ app/src/main/java/com/uchinokomoratte/norokoro/NorouActivity.java
@@ -5,11 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-
+//あぷり
 public class NorouActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_norou);
+
+
     }
 }
