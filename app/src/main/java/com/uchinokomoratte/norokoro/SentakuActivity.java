30d1f991b2df4a00d0600088fbb863b40e14570a
fulukawamoka <shina_yuruha@i.softbank.jp>
2019-02-17 02:51:58 +0900

    選択画面を作り始めた

---
+++ app/src/main/java/com/uchinokomoratte/norokoro/SentakuActivity.java
@@ -0,0 +1,17 @@
+package com.uchinokomoratte.norokoro;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SentakuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sentaku);
+
+    }
+}
--- app/src/main/java/com/uchinokomoratte/norokoro/SplashActivity.java
+++ app/src/main/java/com/uchinokomoratte/norokoro/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent=new Intent(SplashActivity.this, SentakuActivity.class);
                 startActivity(intent);
             }
         }, 3000);
