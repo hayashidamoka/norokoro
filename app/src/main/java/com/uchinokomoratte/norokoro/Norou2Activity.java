042e76cf7d06c0de28e804161dbc596fdfd000b4
fulukawamoka <shina_yuruha@i.softbank.jp>
2019-02-22 00:02:28 +0900

    選択画面で画像を選択すると、プレビューに反映されるようになった

--- app/src/main/java/com/uchinokomoratte/norokoro/Norou2Activity.java
+++ app/src/main/java/com/uchinokomoratte/norokoro/Norou2Activity.java
@@ -9,7 +9,6 @@ public class Norou2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_norou2);
 
 
--- app/src/main/java/com/uchinokomoratte/norokoro/SentakuActivity.java
+++ app/src/main/java/com/uchinokomoratte/norokoro/SentakuActivity.java
@@ -1,22 +1,65 @@
 package com.uchinokomoratte.norokoro;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 public class SentakuActivity extends AppCompatActivity {
+
+    public void d1(View v){
+        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike1);
+    }
+
+    public void d2(View v){
+        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike2);
+    }
+
+    public void d3(View v){
+        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike3);
+    }
+
+    public void d4(View v){
+        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike4);
+    }
+
+    public void d5(View v){
+        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike5);
+    }
+
+    public void d6(View v){
+        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike6);
+    }
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sentaku);
 
-//        public void d1(view v){
-//            ((ImageView))
-//                    findViewById
+        public void start(View v){
+            EditText text =(EditText)findViewById(R.id.name);
+            if(text.getText().toString().isEmpty()){
+                text.setError("なまえいれてよぅ");
+            }
         }
 
+        final ImageView start = findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent();
+                intent.setClass(SentakuActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
+
+
+    }
 }
