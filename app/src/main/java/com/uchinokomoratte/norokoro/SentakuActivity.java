da339293f69ce343c5eaaa365cfcac2c2d91cd31
fulukawamoka <shina_yuruha@i.softbank.jp>
2019-02-27 21:45:15 +0900

    選択画面で選択した画像と名前を保存できるようになった。

--- app/src/main/java/com/uchinokomoratte/norokoro/SentakuActivity.java
+++ app/src/main/java/com/uchinokomoratte/norokoro/SentakuActivity.java
@@ -1,7 +1,9 @@
 package com.uchinokomoratte.norokoro;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,41 +12,72 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
 public class SentakuActivity extends AppCompatActivity {
-
-    public void d1(View v){
-        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike1);
-    }
-
-    public void d2(View v){
-        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike2);
-    }
-
-    public void d3(View v){
-        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike3);
-    }
-
-    public void d4(View v){
-        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike4);
-    }
-
-    public void d5(View v){
-        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike5);
-    }
-
-    public void d6(View v){
-        ((ImageView) findViewById(R.id.dislike)).setImageResource(R.drawable.dislike6);
-    }
-
+    private int gazouNoID;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sentaku);
+        setContentView(R.layout.activity_sentaku);
         final EditText nameEditText = findViewById(R.id.nameEditText);
         final ImageView start = findViewById(R.id.start);
         final TextView ErrorText = findViewById(R.id.ErrorText);
+        final ImageView dislike = findViewById(R.id.dislike);
+
+        ImageView boyImageView = findViewById(R.id.dislike_boy);
+        boyImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dislike.setImageResource(R.drawable.dislike_boy);
+                gazouNoID = R.drawable.dislike_boy;
+            }
+        });
+
+        ImageView girlImageView = findViewById(R.id.dislike_girl);
+        girlImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dislike.setImageResource(R.drawable.dislike_girl);
+                gazouNoID = R.drawable.dislike_girl;
+            }
+        });
+
+        ImageView bareImageView = findViewById(R.id.dislike_bare);
+        bareImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dislike.setImageResource(R.drawable.dislike_bare);
+                gazouNoID = R.drawable.dislike_bare;
+            }
+        });
+
+        ImageView misterImageView = findViewById(R.id.dislike_mister);
+        misterImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dislike.setImageResource(R.drawable.dislike_mister);
+                gazouNoID = R.drawable.dislike_mister;
+            }
+        });
+
+        ImageView ladyImageView = findViewById(R.id.dislike_lady);
+        ladyImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dislike.setImageResource(R.drawable.dislike_lady);
+                gazouNoID = R.drawable.dislike_lady;
+            }
+        });
+
+        ImageView devilImageView = findViewById(R.id.dislike_devil);
+        devilImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dislike.setImageResource(R.drawable.dislike_devil);
+                gazouNoID = R.drawable.dislike_devil;
+            }
+        });
+
         start.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -53,13 +86,26 @@ public class SentakuActivity extends AppCompatActivity {
                 if(name.length() == 0){
                     ErrorText.setText("↓なまえいれてよぅ(´･ω･`)ﾈｪﾈｪ" );
                 }else {
+                    // 画像IDを保存
+                    SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+                    prefs.edit().putInt("GAZOU_NO_ID",gazouNoID).apply();
+                    prefs.edit().putString("DISLIKE_NAME",name).apply();
                     Intent intent = new Intent();
                     intent.setClass(SentakuActivity.this,MainActivity.class);
                     startActivity(intent);
                 }
+
             }
+
         });
 }
+
+    private void nagao() {
+
+
+
+
+    }
 }
 
 
