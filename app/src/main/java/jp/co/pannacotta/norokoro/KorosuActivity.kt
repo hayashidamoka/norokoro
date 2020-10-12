e122f04b7df7b59c704d8dd8899ad8d1b9342a31
hayashidamoka <sudekamo1827@gmail.com>
2020-10-13 00:25:07 +0900

    殺す画面の文言変更 メイン画面のボタンにシャドウつけた

--- app/src/main/java/jp/co/pannacotta/norokoro/KorosuActivity.kt
+++ app/src/main/java/jp/co/pannacotta/norokoro/KorosuActivity.kt
@@ -1,11 +1,21 @@
 package jp.co.pannacotta.norokoro
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
 
 class KorosuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_korosu)
+
+        val backButton = findViewById<Button>(R.id.back_button)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
